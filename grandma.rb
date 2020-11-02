3a89c77c2dc85278632e81b076f1f034fec7604b
Hannah Malm <hannahemalm616@gmail.com>
2020-11-02 22:01:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,19 @@
+require 'pry'
+
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  binding.pry
+  if phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!" 
+  end
+end
+    
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
