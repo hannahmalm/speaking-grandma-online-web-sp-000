f7c71fdc372d98badc272e1172a2f61d9c140a8d
Hannah Malm <hannahemalm616@gmail.com>
2020-11-03 00:43:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!" 
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else 
+  else phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
